81cb6c9d856c9e7948e63d9b064fb0f42588fe15
AslaevAnton <odeyalov@mail.ru>
2017-08-16 13:57:01 +0300

    16082017

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -7,9 +7,12 @@ package com.javarush.task.task03.task0303;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        System.out.println(convertEurToUsd(2,1.5));
+        System.out.println(convertEurToUsd(3,1.8));
     }
 
     public static double convertEurToUsd(int eur, double course) {
         //напишите тут ваш код
+        return eur*course;
     }
 }
