27f0ad07f666a8c7a7d1f84c1ec8c7aa4dceded5
AslaevAnton <odeyalov@mail.ru>
2017-08-17 10:09:42 +0300

    17.08.2017 10:00

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task03.task0319;
+
+/* 
+Предсказание на будущее
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        //«Имя» получает «число1» через «число2» лет.
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        String sAge1 = reader.readLine();
+        String sAge2 = reader.readLine();
+
+        System.out.println(name+" получает "+sAge1+" через "+sAge2+" лет.");
+
+    }
+}
